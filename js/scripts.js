3067838afeefcad9e80e8eca0de6c6c7e9c59f62
Aaron Durant <aaron@aarondurant.com>
2020-10-20 16:15:18 -0400

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ let pokemonList = [
 	{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
 	{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']},
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.3) {
+	document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow! That's big!" + "</p>");
+  } else {
+	document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "</p>");
+  }
+}

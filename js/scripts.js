8c04190c24bafb6259f4f47d1ec11eed150c8a17
Aaron Durant <aaron@aarondurant.com>
2020-10-19 21:05:41 -0400

    added Pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'French toast';
-document.write(favoriteFood);
+let pokemonList = [
+	{name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']}
+	{name: 'Kadabra', height: 1.3, types: ['psychic', 'human-like']}
+	{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']}
+	{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']]
+]

61de790a19bed585fa968b07000885ada062000f
Aaron Durant <aaron@aarondurant.com>
2020-10-20 10:22:33 -0400

    added commas after array elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-	{name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']}
-	{name: 'Kadabra', height: 1.3, types: ['psychic', 'human-like']}
-	{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']}
-	{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']]
+	{name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
+	{name: 'Kadabra', height: 1.3, types: ['psychic', 'human-like']},
+	{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
+	{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']},
 ]

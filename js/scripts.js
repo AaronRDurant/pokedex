bc7b6d9e6bc0ddc7e32e7aeb5a7d09c3eaf635fd
Aaron Durant <aaron@aarondurant.com>
2020-10-27 15:22:43 -0400

    replaced for loop with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,39 @@
-let pokemonList = [
-	{name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
-	{name: 'Kadabra', height: 1.3, types: ['psychic', 'human-like']},
-	{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
-	{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']},
-]
-
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.3) {
-	document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow! That's big!</p>`);
-  } else {
-	document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) </p>`);
-  }
+let pokemonRepository = (function () {
+	// Array of Pokemon objects
+	let pokemonList = [
+		{name: 'Pikachu', height: 0.4, types: ['electric', 'field', 'fairy']},
+		{name: 'Kadabra', height: 1.3, types: ['psychic', 'human-like']},
+		{name: 'Charizard', height: 1.7, types: ['fire', 'flying', 'monster', 'dragon']},
+		{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison', 'monster', 'grass']},
+	];
+	
+	// Adds new objects to pokemonList
+	function add(pokemon) {
+		// Ensures only objects are added. Bonus task!!!
+		if (typeof pokemon !== "object") {
+			return 0;
+		}
+	}
+	// Pushes new objects after passing the type conditional above
+	pokemonList.push(pokemon);
 }
+
+	// Provides access to data
+	function getAll() {
+		return pokemonList;
+	}
+	
+	// Provides access to functions
+	return {
+		add: add,
+		getAll: getAll,
+	};
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height > 1.3) {
+	document.write(`<p> ${pokemon.name} (height: ${pokemon.height}) - Wow! That's big!</p>`);
+  } else {
+	document.write(`<p> ${pokemon.name} (height: ${pokemon.height}) </p>`);
+  }
+});
